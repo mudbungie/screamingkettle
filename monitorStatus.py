6a18f01fc05e90a2b7ca2673b7cba8f0eb351395
mudbungie <mudbungie@gmail.com>
2015-11-22 04:59:08 -0800

    working base site; had to rename status for namespace reasons

--- monitorStatus.py
+++ monitorStatus.py
@@ -1,12 +1,14 @@
 import datetime, pytz
 
+statusFilesPath = 'statuses/'
+
 # parent class for statuses
 # not for implementation on its own; needs to get values from child classes
 class status:
-    def __init__():
+    def __init__(self):
         self.values = {}
 
-    def record(title):
+    def record(self, title):
         # writes the status to a file for use by the webserver
         timestamp = datetime.datetime.now(tz=pytz.utc).strftime()
         with open('statuses/' + title, 'w') as statusFile:
@@ -17,10 +19,10 @@ class status:
 
 class recordedStatus(status):
     # for repopulating a status from a file
-    def __init__(title):
-    self.values = {}
-    with open('statuses/' + title, 'r') as statusFile:
-            # break on the first '=' sign to establish key-value pairs for a dictionary
-            for line in statusFile.read():
-                lineSplit = line.split('=', 1)
-                values[lineSplit[0]] = lineSplit[1])
+    def __init__(self, fileName):
+        self.values = {'service': fileName.split('/')[-1]}
+        with open(statusFilesPath + fileName) as statusFile:
+                # break on the first '=' sign to establish key-value pairs for a dictionary
+                for line in statusFile.read().splitlines():
+                    lineSplit = line.split('=', 1)
+                    self.values[lineSplit[0]] = lineSplit[1]
--- readout.py
+++ readout.py
@@ -7,14 +7,36 @@
 from bottle import route, run, template
 import datetime, pytz
 import os.path
-import status
+import monitorStatus
 
 @route('/')
 def index():
-    # crawl the 
-    for statusFile in os.listdir('status'):
-        with open()
-    response
+    statuses = []
+    response =  '<body>\n'
+    response += '<table>\n'
+    response += '<tr><td>Service</td><td>Status</td><td>Notes</td></tr>\n'
+    # for adding each status to the table
+    def appendStatus(fieldName):
+        try:
+            row =  '<td>' + fieldName + '</td>'
+        # to keep things aligned in case a field is empty
+        except KeyError:
+            row =  '<td></td><td></td>'
+        return row
+    # crawl the statuses directory for all the things that I'm watching
+    for listing in os.listdir(monitorStatus.statusFilesPath):
+        a = monitorStatus.recordedStatus(listing)
+        statuses.append(a)
+    # add the contents of those statuses to the response 
+    for status in statuses:
+        response += '<tr>'
+        response += '<td>' + status.values['service'] + '</td>'
+        response += '<td>' + status.values['status'] + '</td>'
+        response += '<td>' + status.values['notes'] + '</td>'
+        response += '</tr>\n'
+
+    response += '</table>\n'
+    response += '</body>'
     return response
 
 run(host='0.0.0.0', port=8000)
