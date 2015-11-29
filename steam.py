df5b0cab1a14504dc2d6dc86a92634a670d74fb0
mudbungie <mudbungie@gmail.com>
2015-11-29 02:12:39 -0800

    added steam.py, for providing accessible remote server checks over HTTP

---
+++ steam.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# takes a single argument from the command line, which is a shell command
+# executes that command upon HTTP request, posts the output as the response
+# you'll usually want to enclose the command in quotes to make it a single
+# argument.
+
+from bottle import route, run
+from sys import argv
+from subprocess import check_output
+
+@route('/')
+def index():
+    command = argv[1]
+    response = check_output(command).decode()
+
+    return response
+
+run(host='0.0.0.0', port=8001)
